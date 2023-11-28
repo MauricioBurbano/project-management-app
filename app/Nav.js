f900e504939c5d59d7d0cf3b17538efe239436f6
mauri <morocobt@gmail.com>
2023-11-28 13:01:18 -0500

    style the nav bar and the project section

--- app/Nav.js
+++ app/Nav.js
@@ -1,5 +1,3 @@
-const buttonStyle = ' w-full mt-3'
-
 function Nav({projects, selectedProject, updateSelectedProject, newProject, updateNewProject}) {
     function handleClick() {
         if (!newProject) updateNewProject()
@@ -19,15 +17,21 @@ function Nav({projects, selectedProject, updateSelectedProject, newProject, upda
         <div className="flex flex-row justify-center">
             <button 
                 onClick={handleClick} 
-                className="basi-1/3 bg-orange-200 hover:bg-orange-300 rounded-full px-8 py-1"
+                className="basi-1/3 bg-orange-200 hover:bg-orange-300 rounded-full px-8 py-1 mb-4"
                 >Add Project
             </button>
         </div>
-        {projects.map(project => <button
-                className={selectedProject === project.id ? 'bg-orange-200' + buttonStyle : 'bg-orange-100 hover:bg-orange-300' + buttonStyle}
-                onClick={() => {selectProject(project.id)}}
-                key={project.id}>{project.title}
-        </button>)}
+        <div className="flex flex-col gap-3">
+            {projects.map(project => <div className="flex flex-row justify-center" key={project.id}>
+                <button
+                    className={'w-3/4 text-left px-3 py-1 rounded-md' + ' ' + (selectedProject === project.id ? 
+                        'bg-orange-200' : 
+                        'bg-orange-100 hover:bg-orange-300')}
+                    onClick={() => {selectProject(project.id)}}
+                    >{project.title}
+                </button>
+            </div>)}
+        </div>
     </>
 }
 
--- app/Project.js
+++ app/Project.js
@@ -18,29 +18,56 @@ function Project({
         task.current.value = ''
     }
 
-    return <div style={{display: selectedProject !== -1 && !newProject ? 'block' : 'none'}}>
-        <button 
-            className="bg-orange-200 hover:bg-orange-300 px-8 py-1 rounded-full"
-            onClick={delSelectedProject}
-            >Delete
-        </button>
-        <p className="font-bold">{project.title}</p>
-        <p>{project.dueDate}</p>
-        <p>{project.description}</p>
-        <p className="font-bold">Tasks</p>
-        <form onSubmit={handleForm}>
-            <input ref={task} type="text" placeholder="Task"/>
-            <button
-                type="submit"
-                className="bg-orange-200 hover:bg-orange-300 px-8 py-1 rounded-full"
-                >Add
-            </button>
-        </form>
-        {tasks.filter(task => task.projectId === selectedProject).map(task => <div key={task.id}>
-            <p className="inline-block">{task.task}</p>
-            <button className="bg-orange-200 hover:bg-orange-300 py-1 px-8 rounded-full" onClick={() => {delTask(task.id)}}>Clear</button>
-        </div>)}
-    </div>
+        return <div className="flex flex-row justify-center">
+            <div 
+                style={{
+                    display: selectedProject !== -1 && !newProject ? 'block' : 'none',
+                    height: '90vh',
+                    width: '90%'
+                }}>
+                <div className="flex flex-row justify-between">
+                    <p className="font-medium text-2xl">{project.title}</p>
+                    <button 
+                        style={{width: '75px'}}
+                        className="bg-orange-200 hover:bg-orange-300 rounded-full"
+                        onClick={delSelectedProject}
+                        >Delete
+                    </button>
+                </div>
+                <p>{project.dueDate}</p>
+                <p>{project.description}</p>
+                <div className="border-t-4 mt-2 border-orange-300 pt-1">
+                    <p className="font-medium text-lg">Tasks</p>
+                    <form className="flex flex-row mb-3 mt-1" onSubmit={handleForm}>
+                        <div className="basis-full mr-3 flex flex-col justify-center">
+                            <input className="w-full rounded-md py-1 px-2" ref={task} type="text" placeholder="Task"/>
+                        </div>
+                        <button
+                            style={{width: '75px'}}
+                            type="submit"
+                            className="bg-orange-200 hover:bg-orange-300 rounded-full"
+                            >Add
+                        </button>
+                    </form>
+                    {tasks.filter(task => task.projectId === selectedProject).map(task => 
+                        <div className="flex flex-row justify-between my-2 px-2 rounded-md bg-orange-50" key={task.id}>
+                            <div className="flex flex-col justify-center">    
+                                <p>{task.task}</p>
+                            </div>
+                            <button 
+                                style={{
+                                    width: '29px',
+                                    height: '29px'
+                                }}
+                                className="relative bg-orange-200 hover:bg-orange-300 my-1 mx-1 rounded-full" 
+                                onClick={() => {delTask(task.id)}}
+                                ><div className="absolute bottom-0 left-1 text-3xl">{String.fromCharCode(0x00D7)}</div>
+                            </button>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div> 
 }
 
 export default Project
